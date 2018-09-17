bc7ce0f430213a6ace67ec98396eb4343ef4699a
Tak Hyungmin <milkho11@naver.com>
2018-09-17 18:13:28 +0900

    start_review_write_view

---
+++ K_SPOT/app/src/main/java/k_spot/jnm/k_spot/ReviewWriteActivity.kt
@@ -0,0 +1,12 @@
+package k_spot.jnm.k_spot
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ReviewWriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_write)
+    }
+}
