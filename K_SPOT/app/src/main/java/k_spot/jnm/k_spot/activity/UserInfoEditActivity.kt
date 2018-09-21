39ebd0bc70d458ba7375d2d2aff37f6a87655377
Younanee <nanyounan@naver.com>
2018-09-21 16:22:13 +0900

    complete user info edit page

---
+++ K_SPOT/app/src/main/java/k_spot/jnm/k_spot/activity/UserInfoEditActivity.kt
@@ -0,0 +1,13 @@
+package k_spot.jnm.k_spot.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import k_spot.jnm.k_spot.R
+
+class UserInfoEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info_edit)
+    }
+}
