8467931c0adfd2e1d91d329d33e09e503e1e740f
Younanee <nanyounan@naver.com>
2018-09-21 16:24:44 +0900

    repaire main frag

--- K_SPOT/app/src/main/java/k_spot/jnm/k_spot/fragment/MainPageFragment.kt
+++ K_SPOT/app/src/main/java/k_spot/jnm/k_spot/fragment/MainPageFragment.kt
@@ -49,7 +49,7 @@ class MainPageFragment : Fragment() {
 
         // ViewPager 생성 function
 //        makeViewPager(view)
-        //getMainFragResonse(view)
+        getMainFragResonse(view)
 
 //        // Auto Scroll
 //        MainFragViewPagerImageSliderAdapter!!.notifyDataSetChanged()
@@ -58,145 +58,145 @@ class MainPageFragment : Fragment() {
 
         return view
     }
-//
-//    override fun onResume() {
-//        super.onResume()
-//
-//        val handler = Handler()
-//        handler.postDelayed(Runnable {
-//            // Auto Scroll
-//            MainFragViewPagerImageSliderAdapter!!.notifyDataSetChanged()
-//            main_page_fragment_viewpager!!.setInterval(5000)
-//            main_page_fragment_viewpager!!.startAutoScroll(1000)
-//        }, 3000)
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        main_page_fragment_viewpager!!.stopAutoScroll()
-//    }
-//
-//    // ViewPager 생성 function
-//    fun makeViewPager(view: View, mainFragViewPagerItem: ArrayList<Theme> ) {
-//
-//        // Auto Slider Adapter 적용
-//        MainFragViewPagerImageSliderAdapter = MainFragViewPagerImageSliderAdapter(this!!.context!!, mainFragViewPagerItem)
-//        view.main_page_fragment_viewpager.adapter = MainFragViewPagerImageSliderAdapter
-//
-//        // 아래 세 줄 위 세 줄로 대체
-//        //view.homeslider = AutoScrollViewPager(this!!.context!!)
-//        //homeslider!!.setAdapter(MainFragViewPagerImageSliderAdapter)
-//        //setUiPageViewController
-//
-//        // images 배열 사이즈로 인디케이터 사이즈 할당
-//        dotsCount = mainFragViewPagerItem.size
-//
-//        // doutsCount 사이즈만큼의 이미지 뷰 배열 선언
-//        var dots: Array<ImageView?> = arrayOfNulls<ImageView>(dotsCount)
-//        // 인디케이터 점 생성
-//        for (i in 0 until dotsCount) {
-//            dots[i] = ImageView(activity)
-//            dots[i]!!.setImageDrawable(resources.getDrawable(R.drawable.nonselecteditem_dot))
-//
-//            val params = LinearLayout.LayoutParams(
-//                    LinearLayout.LayoutParams.WRAP_CONTENT,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT
-//            )
-//
-//            // 인디케이터 점 마진 설정
-//            params.setMargins(10, 0, 10, 0)
-//            //LinearView에 뷰 생성
-//            view.main_page_fragment_viewpager_count_dot_ll!!.addView(dots[i], params)
-//        }
-//
-//        // 첫번째 indicater를 selecteditem_dot로 활성화
-//        dots[0]!!.setImageDrawable(resources.getDrawable(R.drawable.selecteditem_dot))
-//
-//        // Viewpager가 Chager될 때 마다 인디케이터 점 변환
-//        view.main_page_fragment_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-//
-//            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-//
-//            // 페이지가 선택되었을 때
-//            override fun onPageSelected(position: Int) {
-//                currentPosition = position
-//                for (i in 0 until dotsCount) {
-//                    dots[i]!!.setImageDrawable(resources.getDrawable(R.drawable.nonselecteditem_dot))
-//
-//                }
-//                if (dots != null) {
-//                    dots[currentPosition % dotsCount
-//                    ]!!.setImageDrawable(resources.getDrawable(R.drawable.selecteditem_dot))
-//                }
-//            }
-//
-//            override fun onPageScrollStateChanged(state: Int) {
-//
-//            }
-//        })
-//
-//    }
-//
-//    fun makeCardView(view: View, recyclerView: RecyclerView, dataSet : ArrayList<Main>) {
-//        val mRecyclerView = recyclerView
-////        val mRecyclerView = view.findViewById(R.id.main_page_fragment_rv1) as RecyclerView
-//        mRecyclerView.setHasFixedSize(true)
-//
-//        val mLayoutManager = GridLayoutManager(this.getContext(), 1, GridLayoutManager.HORIZONTAL, false)
-//
-//        mRecyclerView.layoutManager = mLayoutManager
-//
-//        val mAdapter = MainFragCardViewAdapter(this!!.context!!, dataSet)
-//        mRecyclerView.adapter = mAdapter
-//    }
-//
-//
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        toast("메인 페이지")
-//    }
-//
-//    fun getMainFragResonse(view: View) {
-//
-//        networkService = ApplicationController.instance.networkService
-//        val authorization: String = SharedPreferenceController.getAuthorization(context = this!!.context!!)
-//        val getMainFragResponse = networkService.getMainFrag(0, authorization)
-//        getMainFragResponse.enqueue(object : Callback<GetMainFragResponse> {
-//            override fun onFailure(call: Call<GetMainFragResponse>?, t: Throwable?) {
-//            }
-//
-//            override fun onResponse(call: Call<GetMainFragResponse>?, response: Response<GetMainFragResponse>?) {
-//                if (response!!.isSuccessful) {
-//                    mainFragViewPagerItem = ArrayList()
-//
-//                    mainFragRecommendSpotRecyclerItem = ArrayList()
-//                    mainFragBestPlaceItem = ArrayList()
-//                    mainFragEventItem = ArrayList()
-//
-//                    mainFragViewPagerItem = response!!.body()!!.data!!.theme
-//
-//                    mainFragRecommendSpotRecyclerItem = response!!.body()!!.data!!.main_recommand_spot
-//                    mainFragBestPlaceItem = response!!.body()!!.data!!.main_best_place
-//                    mainFragEventItem = response!!.body()!!.data!!.main_best_event
-//
-//                    val mRecyclerView1 = view.findViewById(R.id.main_page_fragment_rv1) as RecyclerView
-//                    // 첫 번째 CardView 생성 function
-//                    makeCardView(view, mRecyclerView1, mainFragRecommendSpotRecyclerItem)
-//
-//                    val mRecyclerView2 = view.findViewById(R.id.main_page_fragment_rv2) as RecyclerView
-//
-//                    makeCardView(view, mRecyclerView2, mainFragRecommendSpotRecyclerItem)
-//
-//                    val mRecyclerView3 = view.findViewById(R.id.main_page_fragment_rv3) as RecyclerView
-//
-//                    makeCardView(view, mRecyclerView3, mainFragEventItem)
-//                    makeViewPager(view ,mainFragViewPagerItem)
-//
-//                }
-//            }
-//
-//        })
-//    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            // Auto Scroll
+            MainFragViewPagerImageSliderAdapter!!.notifyDataSetChanged()
+            main_page_fragment_viewpager!!.setInterval(5000)
+            main_page_fragment_viewpager!!.startAutoScroll(1000)
+        }, 3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        main_page_fragment_viewpager!!.stopAutoScroll()
+    }
+
+    // ViewPager 생성 function
+    fun makeViewPager(view: View, mainFragViewPagerItem: ArrayList<Theme> ) {
+
+        // Auto Slider Adapter 적용
+        MainFragViewPagerImageSliderAdapter = MainFragViewPagerImageSliderAdapter(this!!.context!!, mainFragViewPagerItem)
+        view.main_page_fragment_viewpager.adapter = MainFragViewPagerImageSliderAdapter
+
+        // 아래 세 줄 위 세 줄로 대체
+        //view.homeslider = AutoScrollViewPager(this!!.context!!)
+        //homeslider!!.setAdapter(MainFragViewPagerImageSliderAdapter)
+        //setUiPageViewController
+
+        // images 배열 사이즈로 인디케이터 사이즈 할당
+        dotsCount = mainFragViewPagerItem.size
+
+        // doutsCount 사이즈만큼의 이미지 뷰 배열 선언
+        var dots: Array<ImageView?> = arrayOfNulls<ImageView>(dotsCount)
+        // 인디케이터 점 생성
+        for (i in 0 until dotsCount) {
+            dots[i] = ImageView(activity)
+            dots[i]!!.setImageDrawable(resources.getDrawable(R.drawable.nonselecteditem_dot))
+
+            val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            // 인디케이터 점 마진 설정
+            params.setMargins(10, 0, 10, 0)
+            //LinearView에 뷰 생성
+            view.main_page_fragment_viewpager_count_dot_ll!!.addView(dots[i], params)
+        }
+
+        // 첫번째 indicater를 selecteditem_dot로 활성화
+        dots[0]!!.setImageDrawable(resources.getDrawable(R.drawable.selecteditem_dot))
+
+        // Viewpager가 Chager될 때 마다 인디케이터 점 변환
+        view.main_page_fragment_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            // 페이지가 선택되었을 때
+            override fun onPageSelected(position: Int) {
+                currentPosition = position
+                for (i in 0 until dotsCount) {
+                    dots[i]!!.setImageDrawable(resources.getDrawable(R.drawable.nonselecteditem_dot))
+
+                }
+                if (dots != null) {
+                    dots[currentPosition % dotsCount
+                    ]!!.setImageDrawable(resources.getDrawable(R.drawable.selecteditem_dot))
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
+    }
+
+    fun makeCardView(view: View, recyclerView: RecyclerView, dataSet : ArrayList<Main>) {
+        val mRecyclerView = recyclerView
+//        val mRecyclerView = view.findViewById(R.id.main_page_fragment_rv1) as RecyclerView
+        mRecyclerView.setHasFixedSize(true)
+
+        val mLayoutManager = GridLayoutManager(this.getContext(), 1, GridLayoutManager.HORIZONTAL, false)
+
+        mRecyclerView.layoutManager = mLayoutManager
+
+        val mAdapter = MainFragCardViewAdapter(this!!.context!!, dataSet)
+        mRecyclerView.adapter = mAdapter
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        toast("메인 페이지")
+    }
+
+    fun getMainFragResonse(view: View) {
+
+        networkService = ApplicationController.instance.networkService
+        val authorization: String = SharedPreferenceController.getAuthorization(context = this!!.context!!)
+        val getMainFragResponse = networkService.getMainFrag(0, authorization)
+        getMainFragResponse.enqueue(object : Callback<GetMainFragResponse> {
+            override fun onFailure(call: Call<GetMainFragResponse>?, t: Throwable?) {
+            }
+
+            override fun onResponse(call: Call<GetMainFragResponse>?, response: Response<GetMainFragResponse>?) {
+                if (response!!.isSuccessful) {
+                    mainFragViewPagerItem = ArrayList()
+
+                    mainFragRecommendSpotRecyclerItem = ArrayList()
+                    mainFragBestPlaceItem = ArrayList()
+                    mainFragEventItem = ArrayList()
+
+                    mainFragViewPagerItem = response!!.body()!!.data!!.theme
+
+                    mainFragRecommendSpotRecyclerItem = response!!.body()!!.data!!.main_recommand_spot
+                    mainFragBestPlaceItem = response!!.body()!!.data!!.main_best_place
+                    mainFragEventItem = response!!.body()!!.data!!.main_best_event
+
+                    val mRecyclerView1 = view.findViewById(R.id.main_page_fragment_rv1) as RecyclerView
+                    // 첫 번째 CardView 생성 function
+                    makeCardView(view, mRecyclerView1, mainFragRecommendSpotRecyclerItem)
+
+                    val mRecyclerView2 = view.findViewById(R.id.main_page_fragment_rv2) as RecyclerView
+
+                    makeCardView(view, mRecyclerView2, mainFragRecommendSpotRecyclerItem)
+
+                    val mRecyclerView3 = view.findViewById(R.id.main_page_fragment_rv3) as RecyclerView
+
+                    makeCardView(view, mRecyclerView3, mainFragEventItem)
+                    makeViewPager(view ,mainFragViewPagerItem)
+
+                }
+            }
+
+        })
+    }
 
 
 }
