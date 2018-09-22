5f9a70ce7032596a7fb45aa2d38f4c9329cefebb
Tak Hyungmin <milkho11@naver.com>
2018-09-22 19:25:19 +0900

    network_Search_result_detail

---
+++ K_SPOT/app/src/main/java/k_spot/jnm/k_spot/SearchEventViewMoreActivity.kt
@@ -0,0 +1,12 @@
+package k_spot.jnm.k_spot
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SearchEventViewMoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_event_view_more)
+    }
+}
