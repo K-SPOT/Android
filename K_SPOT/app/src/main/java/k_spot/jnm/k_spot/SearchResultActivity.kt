2d6702f67a6569eacc325d484266fcbcd1ee5d26
Tak Hyungmin <milkho11@naver.com>
2018-09-23 02:40:06 +0900

    network_search_view_more_act

--- K_SPOT/app/src/main/java/k_spot/jnm/k_spot/SearchResultActivity.kt
+++ K_SPOT/app/src/main/java/k_spot/jnm/k_spot/SearchResultActivity.kt
@@ -1,11 +1,14 @@
 package k_spot.jnm.k_spot
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import k_spot.jnm.k_spot.Get.ChannelSearchResultData
@@ -17,12 +20,22 @@ import k_spot.jnm.k_spot.adapter.SearchResultActBroadRecyclerAdapter
 import k_spot.jnm.k_spot.adapter.SearchResultActSpotRecyclerAdapter
 import k_spot.jnm.k_spot.db.SharedPreferenceController
 import kotlinx.android.synthetic.main.activity_search_result.*
-import org.jetbrains.anko.startActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SearchResultActivity : AppCompatActivity() {
+class SearchResultActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        val id = v!!.getId()
+        Log.v("id",id.toString())
+        val broadIndex: Int = search_result_act_celeb_borad_rv.getChildAdapterPosition(v)
+        if (searchBroadItems.size != 0) {
+            val channel_id = searchBroadItems[broadIndex].channel_id
+            Log.v("channel_id", channel_id.toString())
+        }
+
+
+    }
 
     lateinit var searchBroadItems: ArrayList<ChannelSearchResultData>
     lateinit var searchSpotItems: ArrayList<PlaceSearchResultData>
@@ -38,22 +51,38 @@ class SearchResultActivity : AppCompatActivity() {
         var keyword = intent.getStringExtra("keyword")
         getSearchResult(keyword)
         setStatusBarTransparent()
+        setOnClickListener()
+        setViewId()
     }
 
-    fun setOnClickListener() {
+    @SuppressLint("ResourceType")
+    private fun setViewId() {
+        search_result_act_celeb_borad_rv.id = 1
+    }
+
+    private fun setOnClickListener() {
         search_result_act_celeb_borad_see_more_btn.setOnClickListener {
-            startActivity<SearchBraodViewMoreActivity>("searchBroadItems" to searchBroadItems)
+            var intent2 = Intent(applicationContext, SearchBraodViewMoreActivity::class.java)
+            intent2.putParcelableArrayListExtra("searchBroadItems", searchBroadItems)
+            startActivity(intent2)
         }
-        search_result_act_celeb_borad_see_more_btn.setOnClickListener {
-//            startActivity<SearchSpotViewMoreActivity>()
+        search_result_act_spot_see_more_btn.setOnClickListener {
+            var intent3 = Intent(applicationContext, SearchSpotViewMoreActivity::class.java)
+            intent3.putParcelableArrayListExtra("searchSpotItems", searchSpotItems)
+            startActivity(intent3)
         }
-        search_result_act_celeb_borad_see_more_btn.setOnClickListener {
-            startActivity<SearchEventViewMoreActivity>()
+        search_result_act_event_see_more_btn.setOnClickListener {
+            var intent4 = Intent(applicationContext, SearchEventViewMoreActivity::class.java)
+            intent4.putParcelableArrayListExtra("searchEventItems", searchEventItems)
+            startActivity(intent4)
+        }
+        search_result_act_back_btn.setOnClickListener {
+            finish()
         }
     }
 
-    fun getSearchResult(keyword : String) {
-        var keyword : String = keyword
+    fun getSearchResult(keyword: String) {
+        var keyword: String = keyword
         networkService = ApplicationController.instance.networkService
         val authorization: String = SharedPreferenceController.getAuthorization(context = applicationContext)
         val getSearchResultResponse = networkService.getSearchResult(1, authorization, keyword)
@@ -67,60 +96,61 @@ class SearchResultActivity : AppCompatActivity() {
                     searchBroadItems = ArrayList()
                     searchSpotItems = ArrayList()
                     searchEventItems = ArrayList()
-
-
-
                     searchBroadItems = response!!.body()!!.data!!.channel
-
                     searchSpotItems = response!!.body()!!.data!!.place
                     searchEventItems = response!!.body()!!.data!!.event
 
-                    if(searchBroadItems.size != 0){
+                    if(searchBroadItems.size == 0 && searchSpotItems.size == 0 && searchEventItems.size == 0){
+                        search_result_act_all_scroll_view.visibility = View.GONE
+                        search_result_act_no_search_result_rl.visibility = View.VISIBLE
+                    }
+
+                    if (searchBroadItems.size != 0) {
                         // 첫 번째 CardView 생성 function
-                        if(searchBroadItems.size == 1){
-                            makeRecyclerView(searchBroadItems,1)
-                        }else if(searchBroadItems.size >= 2){
-                            makeRecyclerView(searchBroadItems,2)
+                        if (searchBroadItems.size == 1) {
+                            makeRecyclerView(searchBroadItems, 1)
+                        } else if (searchBroadItems.size >= 2) {
+                            makeRecyclerView(searchBroadItems, 2)
                         }
 
-                        if(searchSpotItems.size == 0 && searchEventItems.size == 0){
+                        if (searchSpotItems.size == 0 && searchEventItems.size == 0) {
                             search_result_act_celeb_borad_under_bar.visibility = View.GONE
                         }
 
-                    }else {
+                    } else {
                         search_result_act_celeb_borad_all_rl.visibility = View.GONE
                     }
-                    if(searchSpotItems.size != 0){
-                        if(searchSpotItems.size == 1){
+                    if (searchSpotItems.size != 0) {
+                        if (searchSpotItems.size == 1) {
                             // 첫 번째 CardView 생성 function
-                            makeRecyclerView1(searchSpotItems,1)
-                        }else if(searchSpotItems.size == 2){
-                            makeRecyclerView1(searchSpotItems,2)
-                        }else if(searchSpotItems.size == 3){
-                            makeRecyclerView1(searchSpotItems,3)
-                        }else if(searchSpotItems.size >= 4){
-                            makeRecyclerView1(searchSpotItems,4)
+                            makeRecyclerView1(searchSpotItems, 1)
+                        } else if (searchSpotItems.size == 2) {
+                            makeRecyclerView1(searchSpotItems, 2)
+                        } else if (searchSpotItems.size == 3) {
+                            makeRecyclerView1(searchSpotItems, 3)
+                        } else if (searchSpotItems.size >= 4) {
+                            makeRecyclerView1(searchSpotItems, 4)
                         }
 
-                        if(searchEventItems.size == 0){
+                        if (searchEventItems.size == 0) {
                             search_result_act_spot_under_bar.visibility = View.GONE
                         }
 
-                    }else {
+                    } else {
                         search_result_act_spot_all_rl.visibility = View.GONE
                     }
-                    if(searchEventItems.size != 0){
-                        if(searchEventItems.size == 1){
+                    if (searchEventItems.size != 0) {
+                        if (searchEventItems.size == 1) {
                             // 첫 번째 CardView 생성 function
-                            makeRecyclerView2(searchEventItems,1)
-                        }else if(searchEventItems.size == 2){
-                            makeRecyclerView2(searchEventItems,2)
-                        }else if(searchEventItems.size == 3){
-                            makeRecyclerView2(searchEventItems,3)
-                        }else if(searchEventItems.size >= 4){
-                            makeRecyclerView2(searchEventItems,4)
+                            makeRecyclerView2(searchEventItems, 1)
+                        } else if (searchEventItems.size == 2) {
+                            makeRecyclerView2(searchEventItems, 2)
+                        } else if (searchEventItems.size == 3) {
+                            makeRecyclerView2(searchEventItems, 3)
+                        } else if (searchEventItems.size >= 4) {
+                            makeRecyclerView2(searchEventItems, 4)
                         }
-                    }else {
+                    } else {
                         search_result_act_event_all_rl.visibility = View.GONE
                     }
                 }
@@ -130,21 +160,21 @@ class SearchResultActivity : AppCompatActivity() {
     }
 
 
-    private fun makeRecyclerView(searchBroadItems : ArrayList<ChannelSearchResultData>, itemCount: Int) {
-        searchResultActBroadRecyclerAdapter = SearchResultActBroadRecyclerAdapter(searchBroadItems, applicationContext,itemCount)
+    private fun makeRecyclerView(searchBroadItems: ArrayList<ChannelSearchResultData>, itemCount: Int) {
+        searchResultActBroadRecyclerAdapter = SearchResultActBroadRecyclerAdapter(searchBroadItems, applicationContext, itemCount, this)
         search_result_act_celeb_borad_rv.layoutManager = LinearLayoutManager(applicationContext)
         search_result_act_celeb_borad_rv.adapter = searchResultActBroadRecyclerAdapter
     }
 
-    private fun makeRecyclerView1(searchSpotItems: ArrayList<PlaceSearchResultData>,itemCount: Int) {
+    private fun makeRecyclerView1(searchSpotItems: ArrayList<PlaceSearchResultData>, itemCount: Int) {
 
-        searchResultActSpotRecyclerAdapter = SearchResultActSpotRecyclerAdapter(searchSpotItems, applicationContext,itemCount)
+        searchResultActSpotRecyclerAdapter = SearchResultActSpotRecyclerAdapter(searchSpotItems, applicationContext, itemCount, 0)
         search_result_act_spot_rv.layoutManager = LinearLayoutManager(applicationContext)
         search_result_act_spot_rv.adapter = searchResultActSpotRecyclerAdapter
     }
 
     private fun makeRecyclerView2(searchEventItems: ArrayList<PlaceSearchResultData>, itemCount: Int) {
-        searchResultActSpotRecyclerAdapter = SearchResultActSpotRecyclerAdapter(searchEventItems, applicationContext,itemCount)
+        searchResultActSpotRecyclerAdapter = SearchResultActSpotRecyclerAdapter(searchEventItems, applicationContext, itemCount, 1)
         search_result_act_event_rv.layoutManager = LinearLayoutManager(applicationContext)
         search_result_act_event_rv.adapter = searchResultActSpotRecyclerAdapter
     }
